0d2508d93dfb406ca2797e3d799529f1b0244035
Gustavo Niemeyer <gustavo@niemeyer.net>
2011-10-09 16:35:19 -0300

    Minor tweak in the package tag line.

--- tomb.go
+++ tomb.go
@@ -26,7 +26,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-// The tomb package supports clean goroutine termination.
+// The tomb package helps with clean goroutine termination.
 //
 // See the Tomb type for details.
 package tomb
